ac946add9b6202330cc18290d0368e2eaec21194
Hardik Jain <hardic19@gmail.com>
2021-04-20 02:44:37 +0530

    added RecyclerView

--- app/src/main/java/com/example/socialapp/Dao/PostDao.kt
+++ app/src/main/java/com/example/socialapp/Dao/PostDao.kt
@@ -1,7 +1,10 @@
 package com.example.socialapp.Dao
 
 import com.example.socialapp.models.Post
+import com.example.socialapp.models.User
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.GlobalScope
@@ -10,19 +13,39 @@ import kotlinx.coroutines.tasks.await
 
 class PostDao {
 
-    private val db = FirebaseFirestore.getInstance()
-    private val postsCollections = db.collection("posts")
-    private val auth = Firebase.auth
+    val db = FirebaseFirestore.getInstance()
+    val postsCollections = db.collection("posts")
+    val auth = Firebase.auth
 
     fun addPost(text: String) {
         GlobalScope.launch {
             val currentUserId = auth.currentUser!!.uid
             val userDao = UserDao()
-            val user = userDao.getUserById(currentUserId).await().toObject(com.example.socialapp.models.User::class.java)!!
+            val user = userDao.getUserById(currentUserId).await().toObject(User::class.java)!!
 
             val currentTime = System.currentTimeMillis()
             val post = Post(text, user, currentTime)
             postsCollections.document().set(post)
+        }
+    }
+
+    fun getPostById(postId: String): Task<DocumentSnapshot> {
+        return postsCollections.document(postId).get()
+    }
+
+    fun updateLikes(postId: String) {
+        GlobalScope.launch {
+            val currentUserId = auth.currentUser!!.uid
+            val post = getPostById(postId).await().toObject(Post::class.java)!!
+            val isLiked = post.likedBy.contains(currentUserId)
+
+            if (isLiked) {
+                post.likedBy.remove(currentUserId)
+            }
+            else {
+                post.likedBy.add(currentUserId)
+            }
+            postsCollections.document(postId).set(post)
         }
     }
 }
