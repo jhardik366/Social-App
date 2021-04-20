cf28a02ecae717535903363ace21318bab838f8f
Hardik Jain <hardic19@gmail.com>
2021-04-21 03:24:10 +0530

    added signout functionality

--- app/src/main/java/com/example/socialapp/MainActivity.kt
+++ app/src/main/java/com/example/socialapp/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.example.socialapp
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.socialapp.Dao.PostDao
 import com.example.socialapp.models.Post
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.Query
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), IPostAdapter {
 
     private lateinit var postDao: PostDao
     private lateinit var adapter: PostAdapter
+    private val TAG = "MainActivity Tag"
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
 
         fab.setOnClickListener {
             val intent = Intent(this, CreatePostActivity::class.java)
@@ -52,5 +65,52 @@ class MainActivity : AppCompatActivity(), IPostAdapter {
 
     override fun onLikeClicked(postId: String) {
         postDao.updateLikes(postId)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.signOut)
+        {
+            Log.i(TAG, "Logout")
+            val builder = AlertDialog.Builder(this)
+            //set title for alert dialog
+            builder.setTitle("Are you sure?")
+            //set message for alert dialog
+            builder.setMessage("Do you want to Signout")
+            builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+            //performing positive action
+            builder.setPositiveButton("Yes") { dialogInterface, which ->
+                signout()
+            }
+            //performing cancel action
+            builder.setNeutralButton("Cancel") { dialogInterface, which ->
+
+            }
+            //performing negative action
+            builder.setNegativeButton("No") { dialogInterface, which ->
+
+            }
+            // Create the AlertDialog
+            val alertDialog: AlertDialog = builder.create()
+            // Set other dialog properties
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun signout() {
+
+        auth.signOut()
+        val signInActivityIntent = Intent(this, SignInActivity::class.java)
+        signInActivityIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(signInActivityIntent)
+        finish()
     }
 }
